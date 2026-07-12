f81502b03e6994fff52b1545c5ce74c0760344dc
Kaedras <6783810+Kaedras@users.noreply.github.com>
2026-07-12 18:30:47 +0200

    Add mod data checker and load order handling to palworld plugin

--- games/game_palworld.py
+++ games/game_palworld.py
@@ -1,3 +1,10 @@
+import math
+from pathlib import Path
+from typing import Iterable, List
+
+import mobase
+
+from ..basic_features import BasicModDataChecker, GlobPatterns
 from ..basic_game import BasicGame
 
 
@@ -8,9 +15,72 @@ class PalworldGame(BasicGame):
 
     GameName = "Palworld"
     GameShortName = "palworld"
+    GameSteamId = 1623730
 
     GameDocumentsDirectory = "%USERPROFILE%/AppData/Local/Pal"
     GameBinary = "Palworld.exe"
-    GameDataPath = "Pal/Content/Paks/~mods"
+    # _ROOT is a placeholder value.
+    # In order to properly apply load order to mods, custom mapping is used below.
+    GameDataPath = "_ROOT"
 
-    GameSteamId = 1623730
+    def init(self, organizer: mobase.IOrganizer) -> bool:
+        super().init(organizer)
+        self._register_feature(
+            BasicModDataChecker(
+                GlobPatterns(
+                    valid=["*.pak"],
+                    ignore=[
+                        "*.mohidden",
+                    ],
+                )
+            )
+        )
+        return True
+
+    def _get_mods_path(self):
+        """The directory path of where .pak files from mods should go"""
+        return (
+            Path(self.gameDirectory().absolutePath())
+            / "Pal"
+            / "Content"
+            / "Paks"
+            / "~mods"
+        )
+
+    def _active_mod_paths(self) -> Iterable[Path]:
+        mods_parent_path = Path(self._organizer.modsPath())
+        modlist = self._organizer.modList().allModsByProfilePriority()
+        for mod in modlist:
+            if self._organizer.modList().state(mod) & mobase.ModState.ACTIVE:
+                yield mods_parent_path / mod
+
+    def _active_mod_mappings(self, mod_paths: List[Path]) -> Iterable[mobase.Mapping]:
+        if not mod_paths:
+            return
+        pak_priority_digits = math.floor(math.log10(len(mod_paths))) + 1
+
+        for priority, mod_path in enumerate(mod_paths):
+            pak_prefix = str(priority).zfill(pak_priority_digits) + "_"
+            for child in mod_path.iterdir():
+                dest_path = (
+                    self._get_mods_path()
+                    / child.with_stem(pak_prefix + child.stem).name
+                )
+                if child.is_dir() or child.suffix.lower() == ".pak":
+                    yield mobase.Mapping(
+                        str(child),
+                        str(dest_path),
+                        child.is_dir(),
+                    )
+
+    def mappings(self) -> List[mobase.Mapping]:
+        return [
+            # Not applying load order modifications to overwrites is OK
+            # since the UX is better this way and it will generally work out anyways
+            mobase.Mapping(
+                self._organizer.overwritePath(),
+                str(self._get_mods_path()),
+                True,
+                True,
+            )
+        ] + list(self._active_mod_mappings(list(self._active_mod_paths())))
